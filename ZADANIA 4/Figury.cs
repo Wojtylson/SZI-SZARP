37ff02dad6e0fb317849a6f6dc3f1d9005505164
Wojtek <146847950+Wojtylson@users.noreply.github.com>
2023-12-10 16:20:05 +0100

    Add files via upload

---
+++ ZADANIA 4/Figury.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB4
+{
+    class Kolo
+    {
+        public static int ile = 0;
+        private double promien;
+
+        //inicjowana z promieniem od razu
+        public Kolo(double r)
+        {
+            promien =r;
+            ile++;
+        }
+
+        //inicjowana bez promienia
+        public Kolo()
+        {
+            ile++;
+        }
+
+        public static double Obwod(double r)
+        {
+            double obw = 2*Math.PI*r;
+            return obw;
+        }
+
+        public double Obwod()
+        {
+            double obw = 2*Math.PI*promien;
+            return obw;
+        }
+
+
+
+    }
+    class Prostokat
+    {
+        public static int ile = 0;
+        private double bok1;
+        private double bok2;
+
+        //inicjowana z bokami od razu
+        public Prostokat(double a,double b)
+        {
+            bok1=a;
+            bok2=b;
+            ile++;
+            
+        }
+        //inicjowana bez boków
+        public Prostokat() 
+        {
+            ile++;
+                }
+
+        public double Obwod()
+        {
+            double obw = bok1+bok1+bok2+bok2;
+            return obw;
+        }
+
+
+        public static int Obwod(int a, int b)
+        {
+            int obw = a+a+b+b;
+            return obw;
+
+
+        }
+
+
+    }
+}
---
+++ ZADANIA 4/Program.cs
@@ -0,0 +1,173 @@
+﻿
+using LAB4;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        //testKola1();
+        //testProstokat1();
+        //testKola2();
+        //testProstokat2();
+        //testKola3();
+        testProstokat3(); 
+
+
+
+
+
+
+    }
+    static void testKola1()
+        {   
+  
+        double r;
+        Console.WriteLine("Podaj promien kola");
+
+
+        for (int i = 0; i < 1;i++)
+        {
+            if (double.TryParse(Console.ReadLine(), out r)&r>0)
+            {
+
+
+                
+                double obw = Kolo.Obwod(r);
+                Console.WriteLine($"Obwod kola o promieniu {r} wynosi {obw:F2}");
+            }
+            else
+            {
+                Console.WriteLine("Bledne dane");
+                i--;
+            
+            
+            }
+        }
+
+    }
+    static void testProstokat1()
+    {
+
+        int a, b;
+        Console.WriteLine("Podaj pierwszy bok prostokąta");
+        for (int i = 0; i < 1; i++)
+        {
+            if(int.TryParse(Console.ReadLine(),out a)&a>0)
+            {
+                Console.WriteLine("Podaj drugi bok prostokąta");
+                for(int j =0; j < 1; j++)
+                {
+                    if (int.TryParse(Console.ReadLine(), out b)& b>0)
+                    {
+                       
+                        int obw = Prostokat.Obwod(a, b);
+                        Console.WriteLine($"Prostokat ma obwód {obw}");
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bledne dane! ");
+                        j--;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Bledne dane!");
+                i--;
+            }
+        }
+
+    }
+    static void testKola2()
+    {
+
+        double r;
+        Console.WriteLine("Podaj promien kola");
+
+
+        for (int i = 0; i < 1; i++)
+        {
+            if (double.TryParse(Console.ReadLine(), out r)&r>0)
+            {
+
+
+                Kolo k = new Kolo(r);
+                double obw = k.Obwod();
+                Console.WriteLine($"Obwod kola o promieniu {r} wynosi {obw:F2}");
+            }
+            else
+            {
+                Console.WriteLine("Bledne dane");
+                i--;
+
+
+            }
+        }
+
+    }
+    static void testProstokat2()
+    {
+
+        int a, b;
+        Console.WriteLine("Podaj pierwszy bok prostokąta");
+        for (int i = 0; i < 1; i++)
+        {
+            if (int.TryParse(Console.ReadLine(), out a)&a>0)
+            {
+                Console.WriteLine("Podaj drugi bok prostokąta");
+                for (int j = 0; j < 1; j++)
+                {
+                    if (int.TryParse(Console.ReadLine(), out b)& b>0)
+                    {
+                        Prostokat p = new Prostokat(a,b);
+                        double obw = p.Obwod();
+                        Console.WriteLine($"Prostokat ma obwód {obw}");
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bledne dane! ");
+                        j--;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Bledne dane!");
+                i--;
+            }
+        }
+
+    }
+    
+    static void testKola3()
+    {
+        Kolo k1= new Kolo();
+        Kolo k2 = new Kolo(3.4);
+        Kolo k3 = new Kolo(5);
+
+        int ile = Kolo.ile;
+
+        Console.WriteLine($"Liczba stworzonych kół wynosi {ile}");
+    }
+    static void testProstokat3()
+    {
+        Prostokat q, w, e, r, t, y;
+        q = new Prostokat();
+        w = new Prostokat(5,7);
+        e = new Prostokat(3, 9);
+        r = new Prostokat(4, 5);
+        t = new Prostokat(3, 4);
+        y = new Prostokat(23, 5);
+        int ile = Prostokat.ile;
+        Console.WriteLine($"Liczba stworzonych prostokątów wynosi {ile}");
+    }
+
+
+
+}
